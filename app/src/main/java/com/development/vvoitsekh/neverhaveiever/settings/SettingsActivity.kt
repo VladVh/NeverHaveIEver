dd71eeb0f63363aaf2d982e10e3560ea4c9cf661
v.voitsekh <v.voitsekh@gmail.com>
2017-11-21 17:00:27 +0200

    switched to Activity in SettingsActivity(not tested)

--- app/src/main/java/com/development/vvoitsekh/neverhaveiever/settings/SettingsActivity.kt
+++ app/src/main/java/com/development/vvoitsekh/neverhaveiever/settings/SettingsActivity.kt
@@ -1,16 +1,24 @@
 package com.development.vvoitsekh.neverhaveiever.settings
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.NavUtils
 import android.view.MenuItem
 import com.development.vvoitsekh.neverhaveiever.BaseActivity
+import com.development.vvoitsekh.neverhaveiever.util.LocaleHelper
 
 
-class SettingsActivity: BaseActivity(){
+class SettingsActivity: Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         fragmentManager.beginTransaction().replace(android.R.id.content, SettingsFragment()).commit()
     }
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(LocaleHelper.onAttach(newBase!!))
+    }
+
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item?.itemId) {
